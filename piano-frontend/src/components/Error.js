d9d7ea37be88bfc448a176425e818dcfa53040db
villysiu <villysiu@gmail.com>
2021-11-03 22:56:16 -0700

    changing to inline if &&

--- piano-frontend/src/components/Error.js
+++ piano-frontend/src/components/Error.js
@@ -2,13 +2,13 @@ const Error = (props) => {
   console.log(props)
   
   return (
-      (props.errors.length !== 0) ? (
+    <>
+      { props.errors.length > 0 &&
         <h4>
             {props.errors.map((e, idx) => <li key={idx}>{e}</li> ) }
         </h4>
-      ) : (
-        null
-    )
+      }
+    </>
   )
 }
 export default Error
